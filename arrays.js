e70d0901feaddecb1d61a3b29d6b9199988c622e
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-21 15:32:43 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(a, n){
+var addElementToBeginningOfArray = ["foo", 1]
+return addElementToBeginningOfArray
+}
+
+function destructivelyAddElementToBeginningOfArray(a, n){
+var destructivelyAddElementToBeginningOfArray = ["foo",1]
+return destructivelyAddElementToBeginningOfArray
+}
