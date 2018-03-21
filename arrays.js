d3df9642284821b6b6061f975b9146a6f5d03456
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-21 15:41:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,6 @@ return addElementToBeginningOfArray
 function destructivelyAddElementToBeginningOfArray(a, n){
 var destructivelyAddElementToBeginningOfArray = ["foo",1]
 return destructivelyAddElementToBeginningOfArray
+const array = [1]
+  addElementToBeginningOfArray(array, 'foo')
 }
-
-var array = [1]
